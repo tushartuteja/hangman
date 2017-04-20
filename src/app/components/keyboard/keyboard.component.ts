013884921777d906135f6073eb863d1e19e22437
Tushar Tuteja <ttuteja@gmail.com>
2017-04-21 00:00:20 +0530

    work change commot

--- src/app/components/keyboard/keyboard.component.ts
+++ src/app/components/keyboard/keyboard.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-keyboard',
@@ -7,9 +7,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class KeyboardComponent implements OnInit {
 
+	@Input() guess_list;
+	top = ['q','w','e','r','t','y','u','i','o','p'];
+	middle = ['a','s','d','f','g','h','j','k','l'];
+	bottom = ['z','x','c','v','b','n','m'];
   constructor() { }
 
   ngOnInit() {
+  }
+
+
+  class_of(ch) {
+  	if (this.guess_list.indexOf(ch) > 0){
+  		return "guessed";
+  	}else{
+  		return "remaining";
+  	}
   }
 
 }
